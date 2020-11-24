bc7d14cdaea7737b2b8d10e9bcdec1a1a8c565dc
Mohammad Jahangiry <mjahangiry75@gmail.com>
2020-11-24 13:52:44 +0330

    Comment database codes to use later

--- app/src/main/java/io/github/masterj3y/sorna/core/database/AppDatabase.kt
+++ app/src/main/java/io/github/masterj3y/sorna/core/database/AppDatabase.kt
@@ -4,11 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 
-@Database(
-    entities = [],
-    version = 2,
-    exportSchema = false
-)
-abstract class AppDatabase : RoomDatabase() {
-
-}
+//@Database(
+//    entities = [],
+//    version = 2,
+//    exportSchema = false
+//)
+//abstract class AppDatabase : RoomDatabase() {
+//
+//}
